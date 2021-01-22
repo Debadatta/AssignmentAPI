ee48f00d80736db541be9e751db7ee53d06dc166
Debadatt Pradhan <debadattapradhan@gmail.com>
2021-01-22 20:34:19 +0530

    Code updated

--- src/test/java/Features/Station.feature
+++ src/test/java/Features/Station.feature
@@ -4,20 +4,16 @@ Feature: Create Station
 		Given I get the API url
 		When I register station without API key
 		Then I should get Http Response code as 401
-		And I should get response message as 'Invalid API key. Please see http://openweathermap.org/faq#error401 for more info.'
+		And I should get response message as "Invalid API key. Please see http://openweathermap.org/faq#error401 for more info."
 		
 	Scenario: Successfully register two stations with the following details and verify that HTTP response code is 201.
 	
 		Given I get the API url
 		When I register station with details
 		Then I should get Http Response code as 201
-		When I get the stations with ids
-		Then I should get the valid details
+		When I fetch the stations with ids I found the details
 		When I delete both the stations 
 		Then I should get Http Response code as 204
-		When I delete the station again
-		Then I should get Http Response code as 404
-		And I should get response message as "Station not found"
-		
+		When I delete the station again I found the Response code 404001 and message "Station not found"
   
   	
--- src/test/java/StepDefinitions/StepDefinitions.java
+++ src/test/java/StepDefinitions/StepDefinitions.java
@@ -1,32 +1,13 @@
 package StepDefinitions;
 
-
-
-import io.restassured.RestAssured.*;
 import io.restassured.http.Method;
-import io.restassured.matcher.RestAssuredMatchers.*;
-import org.hamcrest.Matchers.*;
-
-import java.util.List;
-import java.util.Map;
- 
-import org.apache.commons.lang3.StringUtils;
-import org.hamcrest.Matcher;
-import org.hamcrest.Matchers;
-
-import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import gherkin.deps.com.google.gson.Gson;
-import gherkin.deps.com.google.gson.JsonObject;
-import gherkin.deps.com.google.gson.JsonParser;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
-
 import org.json.*;
 import org.junit.Assert;
 
@@ -34,38 +15,38 @@ import org.junit.Assert;
 public class StepDefinitions {
 	
 	private Response response;
-	private String json;
 	private RequestSpecification request;
     private String ENDPOINT_URL ;
     private String id1;
     private String id2;
-    List < String > allStationIDs;
+    static final String APPID = "94ee89226ce499d45a755090f02e3e7b"; 
  
 	@Given("I get the API url")
 	public void i_get_api_url(){
 		ENDPOINT_URL = "http://api.openweathermap.org/data/3.0/stations";
-		
+		request = RestAssured.given();
 	}
  
 	@When("I register station without API key")
 	public void i_register_station_without_api_key(){
-		JSONObject requestParams = new JSONObject();
-		requestParams.put("external_id", "DEMO_TEST001"); 
-		requestParams.put("name", "Interview Station <Random Number>");
-		RequestSpecification request = RestAssured.given();
-		requestParams.put("latitude", 33.33);
-		requestParams.put("longitude", -111.43);
-		requestParams.put("altitude",  444);
-		request.header("Content-Type", "application/json");
-		request.body(requestParams.toString());
-		response = request.post(ENDPOINT_URL);
-		System.out.println("response: " + response.getStatusCode());
+		try {
+			JSONObject requestParams = new JSONObject();
+			requestParams.put("external_id", "DEMO_TEST001"); 
+			requestParams.put("name", "Interview Station <Random Number>");
+			
+			requestParams.put("latitude", 33.33);
+			requestParams.put("longitude", -111.43);
+			requestParams.put("altitude",  444);
+			request.header("Content-Type", "application/json");
+			request.body(requestParams.toString());
+			response = request.post(ENDPOINT_URL);
+			System.out.println("response: " + response.getStatusCode());
+		} catch( Exception e ) {
+			System.out.println("Stations created successfully");
+		}
 	}
  
 	
-	
-
-	//@Then("^I should get Http Response code as \"(.*?)\"$")
 	@Then("^I should get Http Response code as (\\d+)$")
 	public void i_should_get_Http_Response_code_as(int statusCode) throws Throwable {
 		int statusCodeActual = response.getStatusCode();
@@ -75,109 +56,103 @@ public class StepDefinitions {
 
 	@Then("^I should get response message as \"(.*?)\"$")
 	public void i_should_get_response_message_as(String message) throws Throwable {
-		JsonParser parser = new JsonParser(); 
-		JsonObject jsonObject = parser.parse(response.body().asString()).getAsJsonObject();
-		System.out.println(jsonObject.get("message").toString());
-		Assert.assertEquals("Actual message recieved", jsonObject.get("message").toString(), message);
+		JsonPath extractor = response.jsonPath();	
+		System.out.println(extractor.get("message").toString());
+		Assert.assertEquals("Actual message recieved", extractor.get("message").toString(), message);
 	}
 	
 	@When("^I register station with details$")
 	public void i_register_station_with_details() {
-		request = RestAssured.given();
-		JSONObject requestParams = new JSONObject();		
+		try {
+			JSONObject requestParams = new JSONObject();		
+				
+			requestParams.put("external_id", "DEMO_TEST001"); 
+			requestParams.put("name", "Interview Station <Random Number>");
+			 
+			requestParams.put("latitude", 33.33);
+			requestParams.put("longitude", -111.43);
+			requestParams.put("altitude",  444);		
 			
-		requestParams.put("external_id", "DEMO_TEST001"); 
-		requestParams.put("name", "Interview Station <Random Number>");
-		 
-		requestParams.put("latitude", 33.33);
-		requestParams.put("longitude", -111.43);
-		requestParams.put("altitude",  444);		
-		
-		JSONObject requestParams1 = new JSONObject();
-		requestParams1.put("external_id", "Interview1"); 
-		requestParams1.put("name", "Interview Station <Random Number>");
-		
-		requestParams1.put("latitude", 33.44);
-		requestParams1.put("longitude", -12.44);
-		requestParams1.put("altitude",  444);
-
-		
-		request.header("Content-Type", "application/json");
-		request.body(requestParams.toString());
-		response = request.request(Method.POST, ENDPOINT_URL+"?APPID=94ee89226ce499d45a755090f02e3e7b");
-		JsonParser parser = new JsonParser(); 
-		JsonObject jsonObject = parser.parse(response.body().asString()).getAsJsonObject();
-		System.out.println(jsonObject.toString());
-		id1 = jsonObject.get("ID").toString();
-		
-		request.body(requestParams1.toString());
-		response = request.request(Method.POST, ENDPOINT_URL+"?APPID=94ee89226ce499d45a755090f02e3e7b");
-		JsonObject jsonObject1 = parser.parse(response.body().asString()).getAsJsonObject();
-		System.out.println(jsonObject1.get("ID").toString());
-		id2 = jsonObject1.get("ID").toString();
-		System.out.println("response: " + response.body().asString());
+			JSONObject requestParams1 = new JSONObject();
+			requestParams1.put("external_id", "Interview1"); 
+			requestParams1.put("name", "Interview Station <Random Number>");
+			
+			requestParams1.put("latitude", 33.44);
+			requestParams1.put("longitude", -12.44);
+			requestParams1.put("altitude",  444);
+	
+			
+			request.header("Content-Type", "application/json");
+			request.body(requestParams.toString());
+			response = request.queryParam("APPID", APPID).request(Method.POST, ENDPOINT_URL);
+			
+			JsonPath extractor = response.jsonPath();	
+			id1 = extractor.get("ID");
+			
+			request.body(requestParams1.toString());
+			response = request.queryParam("APPID", APPID).request(Method.POST, ENDPOINT_URL);
+			JsonPath extractor1 = response.jsonPath();
+			id2 = extractor1.get("ID");
+			System.out.println("Stations created: "+ id1 + ", "+ id2);
+		} catch( Exception e ) {
+			System.out.println("Stations not created");
+		}
 	}
 	
-	@When("^I get the stations with ids$")
+	@When("^I fetch the stations with ids I found the details$")
 	public void i_get_the_stations_with_ids() throws Throwable {
-		request = RestAssured.given().queryParam("APPID", "94ee89226ce499d45a755090f02e3e7b");
-		request.header("Content-Type", "application/json");
-		response = request.request(Method.GET, ENDPOINT_URL+"/"+ id1);	
-		JsonParser parser = new JsonParser(); 
-		JsonObject jsonObject = parser.parse(response.body().asString()).getAsJsonObject();
-		Assert.assertEquals(jsonObject.get("external_id"), "DEMO_TEST001");
-		
+		try {			
+			String strID1 = ENDPOINT_URL+"/"+ id1;
+			response = request.queryParam("APPID", APPID).request(Method.GET,  strID1);
 			
-		request = RestAssured.given().queryParam("APPID", "94ee89226ce499d45a755090f02e3e7b");
-		request.header("Content-Type", "application/json");
-		response = request.request(Method.GET, ENDPOINT_URL+"/"+ id2);	
-		JsonParser parser1 = new JsonParser(); 
-		JsonObject jsonObject1 = parser1.parse(response.body().asString()).getAsJsonObject();
-		Assert.assertEquals(jsonObject1.get("external_id"), "Interview1");
-		
-	}
-
-	@Then("^I should get the valid details$")
-	public void i_should_get_the_valid_details() throws Throwable {
-		if(response.prettyPrint().contains("DEMO_TEST001")) {
-			System.out.println("DEMO_TEST001 record present");
-		} else {
-			System.out.println("DEMO_TEST001 record not present");
+			JsonPath extractor = response.jsonPath();		
+			Assert.assertEquals(extractor.get("external_id"), "DEMO_TEST001");		
+				
+			request.header("Content-Type", "application/json");
+			String strID2 = ENDPOINT_URL+"/"+ id2;	
+			response = request.queryParam("APPID", APPID).request(Method.GET,  strID2);
+			
+			JsonPath extractor1 = response.jsonPath();		
+			Assert.assertEquals(extractor1.get("external_id"), "Interview1");	
+		} catch( Exception e ) {
+			System.out.println("Stations not found");
 		}
 		
-		if(response.prettyPrint().contains("Interview1")) {
-			System.out.println("Interview1 record present");
-		} else {
-			System.out.println("Interview1 record not present");
-		}
 	}
 
 	@When("^I delete both the stations$")
 	public void i_delete_both_the_stations() throws Throwable {
-		JSONObject requestParams = new JSONObject();
-		requestParams.put("appid", "94ee89226ce499d45a755090f02e3e7b");
-		requestParams.put("id", allStationIDs.get(0));
-		request.header("Content-Type", "application/json");
-		request.body(requestParams.toString());
-		response = request.delete(ENDPOINT_URL);
+		try {
+			String strID1 = ENDPOINT_URL+"/"+ id1;
+			response = request.queryParam("APPID", APPID).request(Method.DELETE, strID1);			
+			
+			String strID2 = ENDPOINT_URL+"/"+ id2;
+			response = request.queryParam("APPID", APPID).request(Method.DELETE, strID2);
+		} catch ( Exception e ) {
+			System.out.println("Stations could not deleted");
+		}
 		
-		
-		JSONObject requestParams1 = new JSONObject();
-		requestParams1.put("appid", "94ee89226ce499d45a755090f02e3e7b");
-		requestParams1.put("id", allStationIDs.get(1));
-		request.header("Content-Type", "application/json");
-		request.body(requestParams1.toString());
-		response = request.delete(ENDPOINT_URL);
 	}
 
-	@When("^I delete the station again$")
-	public void i_delete_the_station_again() throws Throwable {
-		JSONObject requestParams = new JSONObject();
-		requestParams.put("appid", "94ee89226ce499d45a755090f02e3e7b");
-		requestParams.put("id", allStationIDs.get(0));
-		request.header("Content-Type", "application/json");
-		request.body(requestParams.toString());
-		response = request.delete(ENDPOINT_URL);
+	@When("^I delete the station again I found the Response code (\\d+) and message \"(.*?)\"$")
+	public void i_delete_the_station_again(int code, String message) throws Throwable {
+		try {
+			String strID1 = ENDPOINT_URL+"/"+ id1;
+			response = request.queryParam("APPID", APPID).request(Method.DELETE, strID1);	
+			JsonPath extractor = response.jsonPath();	
+			Assert.assertEquals(extractor.get("code"), code );	
+			Assert.assertEquals(extractor.get("message"), message );
+			
+			String strID2 = ENDPOINT_URL+"/"+ id2;
+			response = request.queryParam("APPID", APPID).request(Method.DELETE, strID2);
+			JsonPath extractor1 = response.jsonPath();	
+			Assert.assertEquals(extractor1.get("code"), code);	
+			Assert.assertEquals(extractor1.get("message"), message);
+			
+		} catch ( Exception e ) {
+			System.out.println("Stations could not deleted");
+		}
+		
 	}
 
 }
